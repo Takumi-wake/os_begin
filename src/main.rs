eed70343b3776f21d4a3e48cda8a6198337952ad
takumi_wake <random_mine@163.com>
2023-11-01 20:25:59 +0800

    support stack and clear_bss

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,34 @@ mod lang_items;
 use core::arch::global_asm;
 global_asm!(include_str!("entry.asm"));
 
-// fn main() {
-//      // println!("Hello, world!");
-// }
+
+#[no_mangle]
+pub fn rust_main() -> ! {
+    loop {}
+}
+
+fn clear_bss() {
+    extern "C" {
+        fn sbss();
+        fn ebss();
+    }
+    (sbss as usize .. ebss as usize).for_each(|a| {
+        unsafe { (a as *mut u8).write_volatile(0) }
+    });
+}
+
+fn clear_bss_test() {
+    extern "C" {
+        static sbss: usize;
+        static ebss: usize;
+    }
+    unsafe {
+        let mut ptr = sbss as *mut u8;
+        let end = ebss as *mut u8;
+        while ptr < end {
+            ptr.write_volatile(0);
+            ptr = ptr.offset(1);
+        }
+    }
+}
+
